aa8a012522aa1591830b93bbdc60be038ec1621e
Jesus Ledezma <jledezma@student.neumont.edu>
2018-08-14 22:28:41 -0600

    Got the site map working

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -70,113 +70,14 @@
 "use strict";
 
 
-var _googleMap = __webpack_require__(1);
-
-var _googleMap2 = _interopRequireDefault(_googleMap);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
+var url = 'sitemap.json';
+fetch(url).then(function (response) {
+  return response.json();
+}).then(function (data) {
+  document.getElementById('homeLink').href = data.index.url;
+}).catch(function (e) {
+  return console.log(e);
 });
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-var initMap = exports.initMap = function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    mapTypeControl: false,
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13
-  });
-
-  new AutocompleteDirectionsHandler(map);
-};
-
-/**
- * @constructor
-*/
-var AutocompleteDirectionsHandler = function AutocompleteDirectionsHandler(map) {
-  undefined.map = map;
-  undefined.originPlaceId = null;
-  undefined.destinationPlaceId = null;
-  undefined.travelMode = 'WALKING';
-  var originInput = document.getElementById('origin-input');
-  var destinationInput = document.getElementById('destination-input');
-  var modeSelector = document.getElementById('mode-selector');
-  undefined.directionsService = new google.maps.DirectionsService();
-  undefined.directionsDisplay = new google.maps.DirectionsRenderer();
-  undefined.directionsDisplay.setMap(map);
-
-  var originAutocomplete = new google.maps.places.Autocomplete(originInput, { placeIdOnly: true });
-  var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput, { placeIdOnly: true });
-
-  undefined.setupClickListener('changemode-walking', 'WALKING');
-  undefined.setupClickListener('changemode-transit', 'TRANSIT');
-  undefined.setupClickListener('changemode-driving', 'DRIVING');
-
-  undefined.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-  undefined.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-  undefined.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-  undefined.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-  undefined.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-};
-
-// Sets a listener on a radio button to change the filter type on Places
-// Autocomplete.
-AutocompleteDirectionsHandler.prototype.setupClickListener = function (id, mode) {
-  var radioButton = document.getElementById(id);
-  var me = undefined;
-  radioButton.addEventListener('click', function () {
-    me.travelMode = mode;
-    me.route();
-  });
-};
-
-AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function (autocomplete, mode) {
-  var me = undefined;
-  autocomplete.bindTo('bounds', undefined.map);
-  autocomplete.addListener('place_changed', function () {
-    var place = autocomplete.getPlace();
-    if (!place.place_id) {
-      window.alert("Please select an option from the dropdown list.");
-      return;
-    }
-    if (mode === 'ORIG') {
-      me.originPlaceId = place.place_id;
-    } else {
-      me.destinationPlaceId = place.place_id;
-    }
-    me.route();
-  });
-};
-
-AutocompleteDirectionsHandler.prototype.route = function () {
-  if (!undefined.originPlaceId || !undefined.destinationPlaceId) {
-    return;
-  }
-  var me = undefined;
-
-  undefined.directionsService.route({
-    origin: { 'placeId': undefined.originPlaceId },
-    destination: { 'placeId': undefined.destinationPlaceId },
-    travelMode: undefined.travelMode
-  }, function (response, status) {
-    if (status === 'OK') {
-      me.directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-};
 
 /***/ })
 /******/ ]);
--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
-
-import initMap from './googleMap.js'
+let url = '../sitemap.json';
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('homeLink').href = data.index.url;
+    document.getElementById('home').innerHTML= data.index.name;
+    document.getElementById('donateLink').href = data.donate.url;
+    document.getElementById('donate').innerHTML= data.donate.name;
+    document.getElementById('admissionLink').href = data.admission.url;
+    document.getElementById('admission').innerHTML= data.admission.name;
+    document.getElementById('googleMapLink').href = data.googleMap.url;
+    document.getElementById('googleMap').innerHTML= data.googleMap.name;
+    document.getElementById('sponsorsLink').href = data.sponsors.url;
+    document.getElementById('sponsors').innerHTML= data.sponsors.name;
+    document.getElementById('sponsor1Link').href = data.sponsor1.url;
+    document.getElementById('sponsor1').innerHTML= data.sponsor1.name;
+    document.getElementById('sponsor2Link').href = data.sponsor2.url;
+    document.getElementById('sponsor2').innerHTML= data.sponsor2.name;
+    document.getElementById('sponsor3Link').href = data.sponsor3.url;
+    document.getElementById('sponsor3').innerHTML= data.sponsor3.name;
+    document.getElementById('sponsor4Link').href = data.sponsor4.url;
+    document.getElementById('sponsor4').innerHTML= data.sponsor4.name;
+    document.getElementById('sponsor5Link').href = data.sponsor5.url;
+    document.getElementById('sponsor5').innerHTML= data.sponsor5.name;
+  })
+  .catch(e => console.log(e));
